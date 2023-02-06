ca845cb79394cc8df5fcaa6f220b9b55310daa72
Diego Alvarez <dalvarez@amyris.com>
2023-02-05 19:41:37 -0800

    fix path

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/marketing/latest/",
+    publicPath: "marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
