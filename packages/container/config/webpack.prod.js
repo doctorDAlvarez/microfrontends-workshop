3fab5ecb186c20dde922886f9f2df31ae9458374
Diego Alvarez <dalvarez@amyris.com>
2023-02-05 19:02:42 -0800

    fix publicPath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,24 +1,25 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const commonConfig = require("./webpack.common")
+const packageJson = require("../package.json")
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
   ],
-};
+}
 
-module.exports = merge(commonConfig, prodConfig);
+module.exports = merge(commonConfig, prodConfig)
